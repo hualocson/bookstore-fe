76e9f3c9507795e0dabc0df208cab4f793eb1b60
locson <hualocson@gmail.com>
2023-12-07 20:39:02 +0700

    ADD about page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,27 +1,28 @@
-import { useRouter } from "next/router";
-
-
+import MemberCard from "@/components/about/member-card";
 import SectionLayout from "@/components/landing-page/section-layout";
 
 const AboutSection = () => {
-  const router = useRouter();
   return (
-    <SectionLayout className="mt-16 flex flex-col items-center">
-      <p className="text-7xl font-bold uppercase text-primary-400">Book Store</p>
-      <p className="text-5xl font-bold uppercase text-primary-400 mt-8">Student Info</p>
-      <div className="flex items-center w-full mt-8">
-        <div className="w-1/3 h-96 mx-4 flex flex-col rounded-l-lg rounded-r-xl">
-            <p className="text-3xl font-bold uppercase text-primary-400 text-center">Lương Minh Chiến</p>
-            <p className="text-3xl font-bold uppercase text-primary-400 text-center mt-4">20110615</p>
-        </div>
-        <div className="w-1/3 h-96 mx-4 flex flex-col rounded-l-lg rounded-r-xl">
-            <p className="text-3xl font-bold uppercase text-primary-400 text-center">Hứa Lộc Sơn</p>
-            <p className="text-3xl font-bold uppercase text-primary-400 text-center mt-4">20110712</p>
-        </div>
-        <div className="w-1/3 h-96 mx-4 flex flex-col rounded-l-lg rounded-r-xl">
-            <p className="text-3xl font-bold uppercase text-primary-400 text-center">Phạm Phúc Bình</p>
-            <p className="text-3xl font-bold uppercase text-primary-400 text-center mt-4">20110252</p>
-        </div>
+    <SectionLayout className="mt-16 flex flex-col items-center gap-6">
+      <p className="mt-8 text-5xl font-bold text-primary-400">Our Teams</p>
+      <div className="flex w-full items-center justify-between">
+        <MemberCard
+          name={"Lương Minh Chiến"}
+          role={"Backend Developer"}
+          desc={"20110615"}
+        />
+
+        <MemberCard
+          name={"Hứa Lộc Sơn"}
+          role={"Frontend Developer"}
+          desc={"20110712"}
+        />
+
+        <MemberCard
+          name={"Phạm Phúc Bình"}
+          role={"Backend Developer"}
+          desc={"20110252"}
+        />
       </div>
     </SectionLayout>
   );
